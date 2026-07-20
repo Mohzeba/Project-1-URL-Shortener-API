c01e96817b914b46afa7c999d4e3b41da03a82ee
Mohzeba <baigmohzeba@gmail.com>
2026-07-20 14:12:45 +0000

    added one more route

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.database import Base, engine
+from app import models
+from app.routes import router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title = "URL Shortener + Analytics API")
+
+app.include_router(router)
--- app/routes.py
+++ app/routes.py
@@ -29,3 +29,19 @@ def redirect_url(short_code: str, db: Session = Depends(get_db)):
     url_entry.click_count += 1
     db.commit()
     return {"long_url": url_entry.long_url}
+
+@router.get("/{short_code}/stats")
+def get_stats(short_code: str, db: Session = Depends(get_db)):
+    url_entry = db.query(URL).filter(URL.short_code == short_code).first()
+    if not url_entry:
+        raise HTTPException(status_code=404, detail="URL not found")
+    return{
+        "short_code": url_entry.short_code,
+        "long_url": url_entry.long_url,
+        "click_count": url_entry.click_count,
+        "created_at": url_entry.created_at
+    }
+    
+    
+
+
