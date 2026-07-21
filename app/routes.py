727aa492822ba612b8c477534583378b32dd6bc1
Mohzeba <baigmohzeba@gmail.com>
2026-07-21 11:29:44 +0000

    Redis caching done

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,10 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models import URL
 from typing import Optional
+import redis
+import os
+
+redis_client = redis.Redis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
 
 router = APIRouter()
 
@@ -26,28 +30,9 @@ def shorten_url(long_url: str, custom_code: str = None, db: Session = Depends(ge
     db.refresh(new_url)
     return {"short_code": short_code, "long_url": long_url}
 
-@router.get("/{short_code}")
-def redirect_url(short_code: str, db: Session = Depends(get_db)):
-    url_entry = db.query(URL).filter(URL.short_code == short_code).first()
-    if not url_entry:
-        raise HTTPException(status_code=404, detail="URL not found")
-    
-    url_entry.click_count += 1
-    db.commit()
-    return {"long_url": url_entry.long_url}
-
 @router.get("/urls")
 def list_urls(db: Session = Depends(get_db)):
     return db.query(URL).all()
-
-@router.delete("/{short_code}")
-def delete_url(short_code: str, db: Session = Depends(get_db)):
-    url_entry = db.query(URL).filter(URL.short_code == short_code).first()
-    if not url_entry:
-        raise HTTPException(status_code=404, detail="URL not found")
-    db.delete(url_entry)
-    db.commit()
-    return {"detail": f"{short_code} deleted"}
 
 @router.get("/{short_code}/stats")
 def get_stats(short_code: str, db: Session = Depends(get_db)):
@@ -60,7 +45,33 @@ def get_stats(short_code: str, db: Session = Depends(get_db)):
         "click_count": url_entry.click_count,
         "created_at": url_entry.created_at
     }
-    
-    
 
+@router.get("/{short_code}")
+def redirect_url(short_code: str, db: Session = Depends(get_db)):
+    cached_url = redis_client.get(short_code)
+    if cached_url:
+        db.query(URL).filter(URL.short_code == short_code).update(
+            {URL.click_count: URL.click_count + 1}
+        )
+        db.commit
+        return {"long_url": cached_url, "source": "cache"}
+    
+    url_entry = db.query(URL).filter(URL.short_code == short_code).first()
+    if not url_entry:
+        raise HTTPException(status_code=404, detail= "URL not found")
+    
+    url_entry.click_count += 1
+    db.commit()
 
+    redis_client.set(short_code, url_entry.long_url, ex=3600)
+
+    return {"long_url": url_entry.long_url, "source": "database" }
+
+@router.delete("/{short_code}")
+def delete_url(short_code: str, db: Session = Depends(get_db)):
+    url_entry = db.query(URL).filter(URL.short_code == short_code).first()
+    if not url_entry:
+        raise HTTPException(status_code=404, detail="URL not found")
+    db.delete(url_entry)
+    db.commit()
+    return {"detail": f"{short_code} deleted"}
